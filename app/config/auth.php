b275df1d9feeaef3f7ce323d4683e5fb6c6b333c
alirezachali <mr.alirezachali@gmail.com>
2026-08-11 05:41:28 -0800

    مرتب کردن کدها

--- app/config/auth.php
+++ app/config/auth.php
@@ -8,13 +8,10 @@ return [
     |--------------------------------------------------------------------------
     | Authentication Defaults
     |--------------------------------------------------------------------------
-    |
     | This option defines the default authentication "guard" and password
     | reset "broker" for your application. You may change these values
     | as required, but they're a perfect start for most applications.
-    |
     */
-
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
@@ -24,7 +21,6 @@ return [
     |--------------------------------------------------------------------------
     | Authentication Guards
     |--------------------------------------------------------------------------
-    |
     | Next, you may define every authentication guard for your application.
     | Of course, a great default configuration has been defined for you
     | which utilizes session storage plus the Eloquent user provider.
@@ -34,9 +30,7 @@ return [
     | system used by the application. Typically, Eloquent is utilized.
     |
     | Supported: "session"
-    |
     */
-
     'guards' => [
         'web' => [
             'driver' => 'session',
@@ -48,7 +42,6 @@ return [
     |--------------------------------------------------------------------------
     | User Providers
     |--------------------------------------------------------------------------
-    |
     | All authentication guards have a user provider, which defines how the
     | users are actually retrieved out of your database or other storage
     | system used by the application. Typically, Eloquent is utilized.
@@ -58,9 +51,7 @@ return [
     | be assigned to any extra authentication guards you have defined.
     |
     | Supported: "database", "eloquent"
-    |
     */
-
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
@@ -77,7 +68,6 @@ return [
     |--------------------------------------------------------------------------
     | Resetting Passwords
     |--------------------------------------------------------------------------
-    |
     | These configuration options specify the behavior of Laravel's password
     | reset functionality, including the table utilized for token storage
     | and the user provider that is invoked to actually retrieve users.
@@ -89,9 +79,7 @@ return [
     | The throttle setting is the number of seconds a user must wait before
     | generating more password reset tokens. This prevents the user from
     | quickly generating a very large amount of password reset tokens.
-    |
     */
-
     'passwords' => [
         'users' => [
             'provider' => 'users',
@@ -105,13 +93,10 @@ return [
     |--------------------------------------------------------------------------
     | Password Confirmation Timeout
     |--------------------------------------------------------------------------
-    |
     | Here you may define the number of seconds before a password confirmation
     | window expires and users are asked to re-enter their password via the
     | confirmation screen. By default, the timeout lasts for three hours.
-    |
     */
-
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
 ];
--- app/config/mail.php
+++ app/config/mail.php
@@ -1,26 +1,19 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
+    /*--------------------------------------------------------------------------
     | Default Mailer
     |--------------------------------------------------------------------------
-    |
     | This option controls the default mailer that is used to send all email
     | messages unless another mailer is explicitly specified when sending
     | the message. All additional mailers can be configured within the
     | "mailers" array. Examples of each type of mailer are provided.
-    |
     */
-
     'default' => env('MAIL_MAILER', 'log'),
 
-    /*
-    |--------------------------------------------------------------------------
+    /*--------------------------------------------------------------------------
     | Mailer Configurations
     |--------------------------------------------------------------------------
-    |
     | Here you may configure all of the mailers used by your application plus
     | their respective settings. Several examples have been configured for
     | you and you are free to add your own as your application requires.
@@ -32,9 +25,7 @@ return [
     | Supported: "smtp", "sendmail", "mailgun", "ses", "ses-v2",
     |            "postmark", "resend", "log", "array",
     |            "failover", "roundrobin"
-    |
     */
-
     'mailers' => [
 
         'smtp' => [
@@ -99,17 +90,13 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
+    /*--------------------------------------------------------------------------
     | Global "From" Address
     |--------------------------------------------------------------------------
-    |
     | You may wish for all emails sent by your application to be sent from
     | the same address. Here you may specify a name and address that is
     | used globally for all emails that are sent by your application.
-    |
     */
-
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
