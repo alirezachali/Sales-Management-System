aaf423fc872799d692f583f224ed0bf9941f1698
Alireza Chali <mr.alirezachali@gmail.com>
2026-09-18 18:16:37 +0330

    5

--- laravel-app/lang/fa/navbar.php
+++ laravel-app/lang/fa/navbar.php
@@ -3,13 +3,13 @@
 return [
     'sidebar_menu_btn' => 'باز و بستن منو سایدبار',
     'store_def_name' => 'فروشگاه',
-    'lang_label' => 'زبان',
+    'lang_label' => 'زبــــــــــان',
     'lang_title' => 'تغییر زبان',
-    'theme_btn' => 'تم',
+    'theme_btn' => 'تـــــــــــم',
     'theme_light_title' => 'حالت روشن',
     'messages' => 'پیام‌های من',
     'theme_dark_title' => 'حالت تیره',
     'profile' => 'پروفایل',
     'settings' => 'تنظیمات',
-    'logout_btn' => 'خروج از سیستم',
+    'logout_btn' => 'خــــروج از ســــیستـــم',
 ];
